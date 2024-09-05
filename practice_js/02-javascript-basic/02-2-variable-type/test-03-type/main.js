63fc8eba8f5daee0f4d941fe32779c394977bb6f
Jinsu <injnamek@gmail.com>
2024-09-05 11:42:36 +0900

    typ 테스트 해보기, 연산자 활용하여 더해보기

---
+++ practice_js/02-javascript-basic/02-2-variable-type/test-03-type/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let data = 10;
+console.log(data, "is", typeof data, "type data");
+
+data = "홍길동";
+console.log(data, "is", typeof data, "type data");
+
+data = true;
+console.log(data, "is", typeof data, "type data");
+
+data = 10.0;
+console.log(data, "is", typeof data, "type data");
+
+data = "10";
+console.log(data, "is", typeof data, "type data");
+
+let data1 = 10;
+let data2 = 20;
+let data3 = "10";
+let data4 = "20";
+
+console.log(data1 + data2, typeof (data1 + data2));
+console.log(data3 + data4, typeof (data3 + data4));
+console.log(data1 + data3, typeof (data1 + data3));

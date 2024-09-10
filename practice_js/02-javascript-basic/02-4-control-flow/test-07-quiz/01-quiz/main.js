32f93a73a6da42d81a90da1144ac8d15657f2dce
Jinsu <injnamek@gmail.com>
2024-09-10 21:40:20 +0900

    n이하 소수 출력하기 다시 풀어보기

--- practice_js/02-javascript-basic/02-4-control-flow/test-07-quiz/01-quiz/main.js
+++ practice_js/02-javascript-basic/02-4-control-flow/test-07-quiz/01-quiz/main.js
@@ -2,76 +2,53 @@
 /*
   1. 해당 숫자가 소수인지 아닌지를 판별하는 방법
   1-1. 2나 3제외하고 2나 3으로 나누어떨어지지 않는 수 (한계가 있다)
-  1-2. 자기보다 작은 모든 수로 나누어떨어지지 않는 수. 약수가 생기면 break.
+  1-2. 자기보다 작은 모든 수로 나누어떨어지지 않는 수. 약수가 생기면 더이상 i를 증가시키지 않고 문을 탈출(break).
   2. 그 숫자를 템플릿 리터럴에 넣어서 반환하는 방법
 */
 
-// 1-1 방법으로 작성.
+// (1) for문 사용
 /*
   const primeTest = () => {
-    let no = parseInt(prompt("2 이상의 숫자를 입력하세요"));
-    let message = "";
-    if (no === 2 || no === 3 || (no % 2 !== 0 && no % 3 !== 0)) {
-      message = `${no}(은)는 소수입니다.`;
+    let no = prompt("2 이상의 숫자를 입력하세요");
+    let num = parseInt(no);
+    // 기본값. (1) num이 2라면 for문으로 돌릴 수 없다. (2) for문 안의 if문이 실행이 안 되면, 즉, 소수라면 기본값을 출력.
+    let message = `${num}은 소수입니다.`;
+    if (no === null) {
+      // 취소를 선택하면 나오는 메시지.
+      message = "취소를 선택하셨습니다. 다시 입력해보세요.";
+    } else if (num < 2 || !num) {
+      // 2미만의 숫자를 입력하거나, 숫자가 아닌 것을 입력하면 나오는 메시지.
+      message = "2 이상의 숫자를 입력하세요.";
     } else {
-      message = `${no}(은)는 소수가 아닙니다.`;
+      for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+          message = `${num}은 소수가 아닙니다`;
+          break;
+        }
+      }
     }
-
     //message 는 유저에게 출력하고자 하는 문자열을 저장한 변수
     document.querySelector("#result").innerHTML = message;
   };
 */
 
-// 1-2 방법으로 작성.
-// const primeTest = () => {
-//   let no = parseInt(prompt("2 이상의 숫자를 입력하세요"));
-//   let message = "";
-//   if (isNaN(no) || no < 2) {
-//     message = "2 이상의 숫자를 입력하세요.";
-//   } else {
-//     for (let i = 2; i < no; i++) {
-//       if (no % i === 0) {
-//         message = `${no}는 소수가 아닙니다.`;
-//         break; // 만약 break가 없다면, 약수인 i가 나와도 계속 i를 no미만까지 증가시키면서 계산한다.
-//       } else {
-//         message = `${no}는 소수입니다.`;
-//       }
-//     }
-//   }
-//   //message 는 유저에게 출력하고자 하는 문자열을 저장한 변수
-//   document.querySelector("#result").innerHTML = message;
-// };
-
-//
-// const primeTest = () => {
-//   let no = parseInt(prompt("2 이상의 숫자를 입력하세요"));
-//   let message = "";
-//   if (isNaN(no) || no < 2) {
-//     message = "2 이상의 숫자를 입력하세요.";
-//   } else {
-//     message = `${no}는 소수입니다.`;
-//     for (let i = 2; i < no; i++) {
-//       if (no % i === 0) {
-//         message = `${no}는 소수가 아닙니다.`;
-//         break;
-//       }
-//     }
-//   }
-//   //message 는 유저에게 출력하고자 하는 문자열을 저장한 변수
-//   document.querySelector("#result").innerHTML = message;
-// };
-
-// for -> while 문으로 변경하여 작성해보자
+// (2) while문 사용
 const primeTest = () => {
-  let no = parseInt(prompt("2 이상의 숫자를 입력하세요"));
-  let message = `${no}는 소수입니다.`;
-  if (isNaN(no) || no < 2) {
+  let no = prompt("2 이상의 숫자를 입력하세요");
+  let num = parseInt(no);
+  // 기본값. (1) num이 2라면 for문으로 돌릴 수 없다. (2) for문 안의 if문이 실행이 안 되면, 즉, 소수라면 기본값을 출력.
+  let message = `${num}은 소수입니다.`;
+  if (no === null) {
+    // 취소를 선택하면 나오는 메시지.
+    message = "취소를 선택하셨습니다. 다시 입력해보세요.";
+  } else if (num < 2 || !num) {
+    // 2미만의 숫자를 입력하거나, 숫자가 아닌 것을 입력하면 나오는 메시지.
     message = "2 이상의 숫자를 입력하세요.";
   } else {
     let i = 2;
-    while (i < no) {
-      if (no % i === 0) {
-        message = `${no}는 소수가 아닙니다.`;
+    while (i < num) {
+      if (num % i === 0) {
+        message = `${num}은 소수가 아닙니다.`;
         break;
       }
       i++;
--- practice_js/02-javascript-basic/02-4-control-flow/test-07-quiz/02-quiz/main.js
+++ practice_js/02-javascript-basic/02-4-control-flow/test-07-quiz/02-quiz/main.js
@@ -1,8 +1,35 @@
 "use strict";
 
 const primeTest = () => {
-  let no = parseInt(prompt("2 이상의 숫자를 입력하세요"));
+  let no = prompt("2 이상의 숫자를 입력하세요");
+  let num = parseInt(no);
   let message = "";
+  let results = [];
+
+  if (no === null) {
+    message = "취소하셨습니다. 다시 입력해보세요.";
+    // 입력값(no)이 숫자가 아니라면 num은 NaN으로 나온다. NaN은 Falsy 값이므로 !NaN은 true다.
+  } else if (num < 2 || !num) {
+    message = "2 이상의 숫자를 입력하세요.";
+  } else {
+    for (let i = 2; i < num; i++) {
+      // 만약 i가 소수가 아니라면 message를 출력하는 게 아니라, isPrimeNum의 상태만 바꾸기 위해 변수 선언.
+      let isPrimeNum = true;
+      // i가 소수인지 i보다 작은 모든 수로 나누어보자
+      for (let j = 2; j < i; j++) {
+        // i가 소수가 아니라면
+        if (i % j === 0) {
+          isPrimeNum = false;
+          break;
+        }
+      }
+      // 소수가 아닌 i가 삽입됐을 때 내부 for문은 탈출하지만, isPrimeNum은 여전히 false인 상태. 그 상태의 i는 배열에 추가되면 안 된다.
+      if (isPrimeNum) {
+        results.push(i);
+      }
+      message = `입력하신 ${num}미만의 소수는 ${results}입니다.`;
+    }
+  }
 
   document.querySelector("#result").innerHTML = message;
 };
@@ -11,7 +38,7 @@ const primeTest = () => {
     [어떤 문을 써야하는지에 대한 판단이 훈련이다.]
     조건문 (if)
       - 입력받은 숫자가 null이 아니라면 아래의 2개 루프를 실행
-      - null이 아니라면 '취소 버튼에 대한 메시지'
+      - null이라면 '취소 버튼에 대한 메시지'
     (1) 2부터 입력받은 수까지 증가시키는 loop
       - 코드 작성 시점에 몇개의 데이터가 나올지 예측이 안 되기에 나중에 결과를 담을 초기 데이터를 빈배열로 설정.
       - 2부터 증가시키는 loop를 for 문으로 작성

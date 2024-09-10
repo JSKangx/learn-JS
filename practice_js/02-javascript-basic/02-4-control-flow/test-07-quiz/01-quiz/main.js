fe419e8582b3e266165440f7dd9c994e115af37d
Jinsu <injnamek@gmail.com>
2024-09-10 16:26:50 +0900

    소수 구하기 다시 풀기

--- practice_js/02-javascript-basic/02-4-control-flow/test-07-quiz/01-quiz/main.js
+++ practice_js/02-javascript-basic/02-4-control-flow/test-07-quiz/01-quiz/main.js
@@ -1,7 +1,7 @@
 "use strict";
 /*
   1. 해당 숫자가 소수인지 아닌지를 판별하는 방법
-  1-1. 2나 3제외하고 2나 3으로 나누어떨어지지 않는 수 (더 있나..?)
+  1-1. 2나 3제외하고 2나 3으로 나누어떨어지지 않는 수 (한계가 있다)
   1-2. 자기보다 작은 모든 수로 나누어떨어지지 않는 수. 약수가 생기면 break.
   2. 그 숫자를 템플릿 리터럴에 넣어서 반환하는 방법
 */
@@ -26,7 +26,18 @@
 const primeTest = () => {
   let no = parseInt(prompt("2 이상의 숫자를 입력하세요"));
   let message = "";
-  if (no < 2) message = "2이상의 숫자를 입력하세요";
+  if (isNaN(no) || no < 2) {
+    message = "2 이상의 숫자를 입력하세요.";
+  } else {
+    for (let i = 2; i < no; i++) {
+      if (no % i === 0) {
+        message = `${no}는 소수가 아닙니다.`;
+        break;
+      } else {
+        message = `${no}는 소수입니다.`;
+      }
+    }
+  }
 
   //message 는 유저에게 출력하고자 하는 문자열을 저장한 변수
   document.querySelector("#result").innerHTML = message;

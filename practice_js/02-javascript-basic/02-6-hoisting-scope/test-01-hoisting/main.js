89353114ed16b905f2b95c02b88f92a16b9dba38
Jinsu <injnamek@gmail.com>
2024-09-11 16:12:35 +0900

    변수, 함수 호이스팅 테스트

--- practice_js/02-javascript-basic/02-6-hoisting-scope/test-01-hoisting/main.js
+++ practice_js/02-javascript-basic/02-6-hoisting-scope/test-01-hoisting/main.js
@@ -1,5 +1,30 @@
 "use strict";
 
+// var로 선언한 변수 호이스팅 테스트
+console.log(`step1, data1 = ${data1}`); // undefined
+data1 = 20;
+console.log(`step2, data1 = ${data1}`); // 20
+var data1 = 10;
+console.log(`step3, data1 = ${data1}`); // 10
+
+// let, const로 선언한 변수 호이스팅 테스트
+// console.log(data2); // Cannot access 'data2' before initailizaion
+// console.log(data3); // Cannot access 'data2' before initailizaion
+let data2 = 10;
+let data3 = 30;
+
+// 함수 호이스팅 테스트 (선언문) : 가능
+console.log(myFun1()); // "myFun1 call"
+function myFun1() {
+  return "myFun1 call";
+}
+
+// 함수 호이스팅 테스트 (표현식)
+// console.log(myFun2()); // Error. Cannot access 'myFun2' before initialization
+const myFun2 = () => {
+  return "myFun2 call";
+};
+
 // 선언식 함수로 선언하면 호이스팅이 될까?
 console.log(myFun2());
 function myFun2() {

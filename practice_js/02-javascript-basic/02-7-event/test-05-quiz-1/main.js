c5b0feba6585c431d1e799306f1f93bfc64a573c
Jinsu <injnamek@gmail.com>
2024-09-12 16:38:04 +0900

    form quiz 복습

--- practice_js/02-javascript-basic/02-7-event/test-05-quiz-1/main.js
+++ practice_js/02-javascript-basic/02-7-event/test-05-quiz-1/main.js
@@ -1,21 +1,20 @@
 "use strict";
 
-// DOM node 객체 획득
-let formNode = document.getElementById("form");
 let resultNode = document.getElementById("result");
+let formNode = document.getElementById("form");
 
-// 화면에 출력하는 함수
-const printResult = (name, phone, job) => {
-  resultNode.innerHTML = `이름 : ${name} <br> 전화번호 : ${phone} <br> 직업 : ${job}`;
+// 유저 입력 정보를 화면에 출력하는 함수
+const printResult = (msg) => {
+  resultNode.innerHTML = msg;
 };
 
-// 폼을 제출했을 때의 event
+// form submit event 발생시 실행할 코드
 formNode.addEventListener("submit", (e) => {
   e.preventDefault();
-  // 나는 change 핸들러를 썼는데, 그건 개별적 input 에 건 것이라 여기선 적절 X
-  // submit event가 발생했을 때, DOM node 객체를 획득하고, 그 객체의 value를 변수에 할당해서 출력하면 좋다.
-  let name = document.getElementById("name").value;
-  let phoneNum = document.getElementById("phoneNum").value;
-  let job = document.getElementById("job").value;
-  printResult(name, phoneNum, job);
+  let nameNode = document.getElementById("name");
+  let phonNode = document.getElementById("phoneNum");
+  let jobNode = document.getElementById("job");
+
+  let msg = `이름 : ${nameNode.value} <br /> 전화번호 : ${phonNode.value} <br /> 직업 : ${jobNode.value}`;
+  printResult(msg);
 });

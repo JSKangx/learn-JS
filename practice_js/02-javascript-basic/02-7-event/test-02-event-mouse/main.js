2e1c1973050cf1fc39b46fb61b4d7ac0ec262724
Jinsu <injnamek@gmail.com>
2024-09-12 13:07:32 +0900

    mouse event 테스트

---
+++ practice_js/02-javascript-basic/02-7-event/test-02-event-mouse/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+window.addEventListener("load", () => {
+  // click, dblclick, down/up event
+  let button = document.getElementById("button");
+  button.addEventListener("click", () => {
+    console.log("button is clicked");
+  });
+  button.addEventListener("dblclick", () => {
+    console.log("button is double clicked");
+  });
+  button.addEventListener("mousedown", () => {
+    console.log("mouse down");
+  });
+  button.addEventListener("mouseup", () => {
+    console.log("mouse up");
+  });
+
+  // mouse move 이벤트 (유저가 마우스를 이동하는 순간에)
+  let area = document.getElementById("area");
+  let result = document.getElementById("result");
+
+  area.addEventListener("mousemove", (e) => {
+    result.innerHTML = `offset(${e.offsetX}, ${e.offsetY}), page(${e.pageX}, ${e.pageY}))`;
+  });
+
+  // enter, over의 차이점 : 버블링
+  let outer = document.getElementById("outer");
+  let inner = document.getElementById("inner");
+
+  outer.addEventListener("mouseenter", () => {
+    console.log("outer mouse enter");
+  });
+  inner.addEventListener("mouseenter", () => {
+    console.log("inner mouse enter");
+  });
+  outer.addEventListener("mouseover", () => {
+    console.log("outer mouse over");
+  });
+  inner.addEventListener("mouseover", () => {
+    console.log("inner mouse over"); // inner mouse over + outer mouse over
+  });
+});

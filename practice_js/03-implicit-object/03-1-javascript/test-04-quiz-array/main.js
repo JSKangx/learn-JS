abbdf3753bc0215ec42ea88d00c1e21dffdc151c
Jinsu <injnamek@gmail.com>
2024-09-13 15:47:59 +0900

    리스트 추가하기 퀴즈 미완성

---
+++ practice_js/03-implicit-object/03-1-javascript/test-04-quiz-array/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let inputNode = document.getElementById("input");
+let resultNode = document.getElementById("result");
+let list = [];
+
+function add() {
+  list.push(inputNode.value);
+  console.log(list);
+  list.map((value) => {
+    resultNode = `<li>${value}</li>`;
+  });
+}
+
+function map() {}
+
+function filter() {}
+
+function sort() {}
+
+/*
+  1. 배열의 데이터가 모든 함수에서 쓰이니까 전역변수로 빈 배열 선언
+  2. input, result DOM node 획득하여 변수에 할당
+  3. 추가 함수 작성
+    3-1. inputNode에 입력된 value에 접근해 변수에 할당.
+    3-2. 얻은 value를 빈 배열에 추가
+    3-3. 배열을 화면에 출력 (화면 출력함수는 모든 버튼에 적용되니까 따로 만들자.)
+    3-4. 출력하는 함수를 추가하는 함수 안에서 호출
+  4. 대문자 변경 함수
+    4-1. map으로 모든 원소에 대해 함수 실행하고, 그걸 변수(배열)에 할당
+    4-2. 그 변수를 화면에 출력
+  5. 필터 함수
+    5-1. 조건 : 문자열의 길이 > 5
+  6. sort 함수
+*/

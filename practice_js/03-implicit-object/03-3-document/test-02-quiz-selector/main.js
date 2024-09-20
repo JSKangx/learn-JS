8c71c4b72d37e5345da3bd827508452aa169448c
Jinsu <injnamek@gmail.com>
2024-09-20 16:47:10 +0900

    DOM 노드 셀렉터 복습

--- practice_js/03-implicit-object/03-3-document/test-02-quiz-selector/main.js
+++ practice_js/03-implicit-object/03-3-document/test-02-quiz-selector/main.js
@@ -1,17 +1,23 @@
 "use strict";
 
-function sumbit() {
-  // DOM 노드 획득
-  let userName = document.querySelector("#userName").value;
-  let hobbyNodes = document.getElementsByName("hobby");
-  let genderNodes = document.getElementsByName("gender");
-  let resultNode = document.querySelector(".result");
+let formNode = document.getElementById("myForm");
 
-  // for (let i = 0; i < hobbyNodes.length; i++) {
-  //   console.log(hobbyNodes[i].value);
-  //   if (hobbyNodes[i].checked === true) {
-  //     console.log(hobbyNodes.value);
-  //   }
-  // }
-  console.log(hobbyNodes);
-}
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // 기본 이벤트 막기
+
+  // DOM 노드 획득
+  let userName = document.querySelector("#name").value;
+  let hobbyNodes = document.querySelectorAll('input[name="hobby"]:checked');
+  let hobbyResults = "";
+  hobbyNodes.forEach((item) => {
+    hobbyResults += item.value;
+  });
+  let gender = document.querySelector('input[name="gender"]:checked').value;
+  let resultNode = document.querySelector("#result");
+
+  resultNode.innerHTML = `
+  이름 : ${userName} <br />
+  취미 : ${hobbyResults} <br />
+  성별 : ${gender}
+  `;
+});

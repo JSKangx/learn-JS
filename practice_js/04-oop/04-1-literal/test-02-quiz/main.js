9a1f38fdb3fe75814c5e4897c5baf6621f22c5fb
Jinsu <injnamek@gmail.com>
2024-09-23 16:02:34 +0900

    디스코드 채팅 메시지 객체로 만들어보기

---
+++ practice_js/04-oop/04-1-literal/test-02-quiz/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let message = {
+  profileCoverURL:
+    "https://www.google.com/imgres?q=wood&imgurl=https%3A%2F%2Fhips.hearstapps.com%2Fhmg-prod%2Fimages%2Frustic-weathered-wood-logs-royalty-free-image-1654709658.jpg%3Fcrop%3D0.88931xw%3A1xh%3Bcenter%2Ctop%26resize%3D1200%3A*&imgrefurl=https%3A%2F%2Fwww.popularmechanics.com%2Fscience%2Fgreen-tech%2Fa40231102%2Flab-grown-wood%2F&docid=jua1WKKX66hzgM&tbnid=9DLIGhU5riaGNM&vet=12ahUKEwiCpfioudiIAxWEzjQHHdp2AgEQM3oECFUQAA..i&w=1200&h=900&hcb=2&ved=2ahUKEwiCpfioudiIAxWEzjQHHdp2AgEQM3oECFUQAA",
+  userName: "12. 장유진",
+  time: new Date("2024-09-04T10:32:11"),
+  chat: "디버깅 용도로 많이 사용합니다",
+  emoji: "thumbs_up",
+  emojiCount: +9,
+};
+console.log(message.profileCoverURL);
+console.log(message.userName);
+console.log(message.time.toLocaleString());
+console.log(message.chat);
+console.log(message.emoji);
+console.log(message.emojiCount);

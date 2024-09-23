6e6919713c89297384b877d42bcaab4b049278f9
Jinsu <injnamek@gmail.com>
2024-09-23 13:35:36 +0900

    todo 리스트 퀴즈

--- practice_js/03-implicit-object/03-3-document/test-07-quiz-node/main.js
+++ practice_js/03-implicit-object/03-3-document/test-07-quiz-node/main.js
@@ -1,11 +1,21 @@
 "use strict";
 
 function addList() {
-  let toDo = document.getElementById("input").value;
+  // input의 입력값 획득
+  let toDo = document.getElementById("input");
+  // input의 입력값을 받아 텍스트 노드를 생성하여 todoList에 할당
+  let todoList = document.createTextNode(toDo.value);
+  // list 라는 li 태그의 노드 생성
   let list = document.createElement("li");
-  let todoList = document.createTextNode(toDo);
+  // todoList라는 텍스트 노드를 list의 자식으로 삽입
   list.appendChild(todoList);
+  // 결과 보여주는 ul의 노드를 획득
   let resultNode = document.getElementById("result");
+  // li 태그의 기능을 하는 list를 결과 창의 첫 번째 자식으로 삽입.
   resultNode.insertBefore(list, resultNode.childNodes[0]);
-  console.log(toDo);
+  toDo.value = "";
+
+  list.addEventListener("click", function () {
+    resultNode.removeChild(this);
+  });
 }

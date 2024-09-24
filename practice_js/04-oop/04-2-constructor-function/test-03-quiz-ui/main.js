1556b9c64515b6f88576c13ad078bfa102ecbe06
Jinsu <injnamek@gmail.com>
2024-09-24 23:52:17 +0900

    복습하며 주석달기

--- practice_js/04-oop/04-2-constructor-function/test-03-quiz-ui/main.js
+++ practice_js/04-oop/04-2-constructor-function/test-03-quiz-ui/main.js
@@ -1,11 +1,12 @@
 "use strict";
 
 /*
+  0. 퍼블리셔가 시안 마크업을 만들어서 줬다.
   1. 내가 동적으로 마크업을 만들어 내야하는 부분은 어디인가?
-    - main 안에 있는 div다. 채팅이 올라올 때마다 div를 js로 만들어줘야 하니까.
-    - 그래서 div를 지웠다. 동적으로 생성하게 위해서.
+    - main 안에 있는 div다. 채팅이 올라올 때마다 js로 div를 만들어줘야 하니까.
+    - 그래서 div를 지웠다. 동적으로 생성하기 위해서.
   2. 채팅글은 같은 구성이지만 값이 다 다른 것이다. 
-    - 그래서 생성자 함수를 만들자.
+    - 그래서 채팅글 객체를 생성하는 생성자 함수를 만들자.
   3. 작업순서
     (1) form에서 유저 입력 받기
 */
@@ -60,8 +61,8 @@ let msgId = 0;
 */
 
 // 유저 입력 데이터 노드 객체
-let nicknameInputNode = document.getElementById("nicknameInput");
 let idInputNode = document.getElementById("idInput");
+let nicknameInputNode = document.getElementById("nicknameInput");
 let msgInputNode = document.getElementById("msgInput");
 // 입력 데이터가 출력될 노드 객체
 let chatMainNode = document.getElementById("chat-main");
@@ -69,9 +70,9 @@ let chatMainNode = document.getElementById("chat-main");
 // 화면에 출력하는 함수는 길것이 예상되니 따로 작성
 function printMessage(message) {}
 
-// send 버튼에 입력된 이벤트 클릭 함수인 send를 정의
+// msger-send-btn에 입력된 이벤트 클릭 함수인 send를 정의
 function send(e) {
-  // 새로고침 방지
+  // 폼 제출시 새로고침 방지
   e.preventDefault();
   // 유저 입력 데이터 추출
   let id = idInputNode.value;
@@ -79,8 +80,11 @@ function send(e) {
   let msg = msgInputNode.value;
 
   // 유저 입력 유효성 검증
+  // 셋 중 하나라도 입력하지 않으면,
   if (id.trim().length === 0 || nickname.trim().length === 0 || msg.trim().length === 0) {
+    // 알림 창이 뜬다.
     alert("아이디, 닉네임, 메시지를 입력해야 합니다.");
+    // 알림 창이 뜨면 아래의 코드는 전부 실행 중단
     return;
   } else {
   }

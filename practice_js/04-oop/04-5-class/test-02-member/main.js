bff681491ebe8ebeaaf6d3ff2ba7e1fee9bb564b
Jinsu <injnamek@gmail.com>
2024-10-01 16:22:02 +0900

    멤버 복습하며 주석 달기

--- practice_js/04-oop/04-5-class/test-02-member/main.js
+++ practice_js/04-oop/04-5-class/test-02-member/main.js
@@ -4,7 +4,7 @@ class User {
   // class 영역에 멤버 선언
   name;
   sayHello() {
-    // 멤버 함수 내에서 this로 객체 멤버 선언 가능하지만 권장하지는 않음.
+    // 메서드 내에서 this로 객체 프로퍼티 선언 가능하지만 권장하지는 않음.
     // 함수 내에서는 멤버를 활용하는 로직만 작성하길 권장.
     this.address = "Seoul";
     console.log(`Hello, ${this.name}(${this.age}), ${this.address}`);
@@ -13,7 +13,7 @@ class User {
     // 생성자 내에서 this로 멤버 선언이 권장됨.
     this.name = name;
     this.age = age;
-    // 생성자 내에서 함수 정의할 수 있지만, 일반적으로는 클래스 필드에서 정의함.
+    // 생성자 내에서 메서드를 정의할 수 있지만, 일반적으로는 클래스 필드에서 정의함.
     this.sayHello2 = function () {
       // 함수를 생성자 내에, 프로퍼티를 클래스 필드에 작성해도, 접근은 된다.
       console.log(`Hello2, ${this.name}(${this.age}, ${this.address})`);

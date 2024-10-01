7a03cf7d012e54525a36ed5771b962b8e19cd926
Jinsu <injnamek@gmail.com>
2024-10-01 17:12:56 +0900

    클래스 상속 복습하며 주석달기

--- practice_js/04-oop/04-5-class/test-04-inherited/main.js
+++ practice_js/04-oop/04-5-class/test-04-inherited/main.js
@@ -10,27 +10,34 @@ class Shape {
   }
 }
 
+// Rect 클래스는 Shape 클래스에서 멤버를 상속 받는다.
 // 이렇게 하는 것만으로 상위 클래스의 멤버가 그대로 들어온다.
 class Rect extends Shape {
-  // 독자적인 멤버 생성
+  // 추가) 독자적인 멤버 생성
   width = 0;
   height = 0;
 }
 
 let rect1 = new Rect();
+// 상위 클래스에서 상속 받은 멤버
 rect1.name = "사각형";
 rect1.x = 10;
 rect1.y = 10;
+
+// 독자적으로 가지고 있는 멤버
 rect1.width = 100;
 rect1.height = 100;
+
+// 프로토타입을 통해 접근하는 메서드
 rect1.draw(); // 사각형을 (10, 10)에 그립니다.
+
 console.log(rect1); // {name: '사각형', x: 10, y: 10, width: 100, height: 100}
 
 // private, static 멤버도 상속이 될까?
 class Super {
   data1 = 10;
-  #data2 = 20;
-  static data3 = 30;
+  #data2 = 20; // private 멤버
+  static data3 = 30; // static 멤버
 }
 class Sub extends Super {
   static data4 = 40;
@@ -42,7 +49,8 @@ class Sub extends Super {
 let obj = new Sub();
 obj.subFun(); // 일반 데이터는 상속 됨.
 console.log(Super.data3); // static 멤버는 클래스 명으로 접근 가능.
-console.log(Sub.data3); // 상위 클래스의 static 멤버는 상속 가능.
+console.log(Sub.data3); // 상위 클래스의 static 멤버는 하위 클래스에 상속 가능.
+console.log(obj.data3); // 그러나 객체 메모리에는 올라가지 않기에 객체명을 통해 접근 불가능.
 
 // 생성자 연결관계 테스트 (1) : 생성자를 명시하지 않은 경우(디폴트 생성자)
 class Super1 {}
@@ -83,10 +91,11 @@ class Sub3 extends Super3 {
   }
 }
 let obj3 = new Sub3("rect2", 20, 20, 300, 300);
+// Sub3 클래스에서 상위 생성자를 호출하는 구문에 매개변수를 안 넣어줬을 경우
+console.log(obj3); //  {name: undefined, x: undefined, y: undefined, width: 300, height: 300}
 console.log(obj3); // {name: 'rect2', x: 20, y: 20, width: 300, height: 300}
 
 // 생성자 연결관계 테스트 (4) : override
-
 class Shape2 {
   data = 10;
   constructor(name, x, y) {
